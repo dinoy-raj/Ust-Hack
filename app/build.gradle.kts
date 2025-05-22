80d705afc87a2cce64fcdf89b912c6f917ea1162
dinoy <dinoy@strollby.com>
2025-05-22 12:12:23 +0530

    Bump version to 3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         applicationId = "com.dinoy.forkcast"
         minSdk = 31
         targetSdk = 35
-        versionCode = 2
-        versionName = "1.0.1"
+        versionCode = 3
+        versionName = "1.0.2"
         multiDexEnabled = true
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
